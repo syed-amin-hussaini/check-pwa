3309fc973fa9a63909d4eb38e7ffd4b1f20ea968
syed-amin-hussaini <110085196+syed-amin-hussaini@users.noreply.github.com>
2023-09-06 12:30:03 +0500

    changes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@ const withPWA = require('next-pwa')({
   dest: 'public'
 })
 
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
+
 module.exports = withPWA({
   // next.js config
 })
