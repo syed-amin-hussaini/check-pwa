7c5d86aebcef828ee1b96f6e0c35165e642f1f3e
syed-amin-hussaini <110085196+syed-amin-hussaini@users.noreply.github.com>
2023-09-07 10:23:40 +0500

    Changes ins

--- src/middleware.js
+++ src/middleware.js
@@ -4,48 +4,48 @@ import { getToken } from "next-auth/jwt";
 
 
 export default async function middleware(req) {
-  const myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
+  // const myHeaders = new Headers();
+  // myHeaders.append("Content-Type", "application/json");
 
   
-  const requestOptions = {
-    method: "GET",
-    redirect: "follow",
-    headers: myHeaders,
-  };
+  // const requestOptions = {
+  //   method: "GET",
+  //   redirect: "follow",
+  //   headers: myHeaders,
+  // };
 
-  try {
-    const userIpResponse = await fetch("https://json.geoiplookup.io/", requestOptions);
-    const userIpData = await userIpResponse?.json();
-    const userCountry = userIpData?.country_name;
-    // console.log(userCountry)
-    // const token = req.cookies.get("token");
-    let user = req?.cookies.get("user")?.value;
-    if (user) {
-      user = JSON?.parse(user) 
-    }
-    let userDetail = {
-      userStatus:user?.profile_status,
-      userToken:user?.token,
-      userCountry:userCountry,
-    }
-    console.log("Cookie Result")
-    console.log({userDetail})
-    // const sessionMiddleware = await getToken({ req: req, secret: process.env.JWT_SECRET }); console.log('Session in middleware: ', sessionMiddleware)
-    // console.log({sessionMiddleware})
-    if (user === undefined) {
-      return NextResponse.rewrite(new URL('/login', req.url))
-    }
-    if (userCountry === 'Pakistans' && req.url !== '/testing') {
-      return NextResponse.rewrite(new URL('/testing', req.url))
-    }
-    if (user?.profile_status != "complete"  ||  user?.profile_status == undefined || user?.profile_status == null ) {
-      return NextResponse.rewrite(new URL('/profile', req.url));
-    }
+  // try {
+  //   const userIpResponse = await fetch("https://json.geoiplookup.io/", requestOptions);
+  //   const userIpData = await userIpResponse?.json();
+  //   const userCountry = userIpData?.country_name;
+  //   // console.log(userCountry)
+  //   // const token = req.cookies.get("token");
+  //   let user = req?.cookies.get("user")?.value;
+  //   if (user) {
+  //     user = JSON?.parse(user) 
+  //   }
+  //   let userDetail = {
+  //     userStatus:user?.profile_status,
+  //     userToken:user?.token,
+  //     userCountry:userCountry,
+  //   }
+  //   console.log("Cookie Result")
+  //   console.log({userDetail})
+  //   // const sessionMiddleware = await getToken({ req: req, secret: process.env.JWT_SECRET }); console.log('Session in middleware: ', sessionMiddleware)
+  //   // console.log({sessionMiddleware})
+  //   if (user === undefined) {
+  //     return NextResponse.rewrite(new URL('/login', req.url))
+  //   }
+  //   if (userCountry === 'Pakistans' && req.url !== '/testing') {
+  //     return NextResponse.rewrite(new URL('/testing', req.url))
+  //   }
+  //   if (user?.profile_status != "complete"  ||  user?.profile_status == undefined || user?.profile_status == null ) {
+  //     return NextResponse.rewrite(new URL('/profile', req.url));
+  //   }
     
-  } catch (error) {
-    console.error("Error fetching user IP data:", error);
-  }
+  // } catch (error) {
+  //   console.error("Error fetching user IP data:", error);
+  // }
 
   // Allow the request to continue processing
   return NextResponse.next();
